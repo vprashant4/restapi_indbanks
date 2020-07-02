2dc39fd799ce2b760212fd93b8b3df937186517c
Prashant Verma <verma.prashant15@gmail.com>
2020-07-02 22:56:27 +0530

    update setting file

--- restapi/settings.py
+++ restapi/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from datetime import timedelta
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,6 +143,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # if os.getcwd() == '/app':
 #     import dj_database_url
 #     db_from_env = dj_database_url.config(conn_max_age=500)
