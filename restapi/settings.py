eb4f3bb47a94612724546abf6f2eb541ebdc52d7
Prashant Verma <verma.prashant15@gmail.com>
2020-07-02 15:45:05 +0530

    update  setting file

--- restapi/settings.py
+++ restapi/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from datetime import timedelta
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,22 +145,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-# # Parse database configuration from $DATABASE_URL
-# if os.getcwd() == '/app':
-#     import dj_database_url
-#     db_from_env = dj_database_url.config(env='DATABASE_URL', conn_max_age=500)
-#     DATABASES['default'].update(db_from_env)
-#
-#     # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-#     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-#
-#     # Allow all host headers
-#     ALLOWED_HOSTS = ['restapibanks.herokuapp.com']
-#     DEBUG = True
-#
-#     # Static asset configuration
-#     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 #  Set JWT Time Duration for 5 days
 # SIMPLE_JWT = {
