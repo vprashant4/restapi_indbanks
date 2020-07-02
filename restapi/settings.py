344a1f4b7f9cf9996051fae8b263c13bf75e62e3
Prashant Verma <verma.prashant15@gmail.com>
2020-07-02 23:05:03 +0530

    update setting file

--- restapi/settings.py
+++ restapi/settings.py
@@ -143,7 +143,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 # if os.getcwd() == '/app':
 #     import dj_database_url
 #     db_from_env = dj_database_url.config(conn_max_age=500)
