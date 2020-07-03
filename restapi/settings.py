fe25e99deae7b7021d2388f41e9c87ef818a0880
Prashant Verma <verma.prashant15@gmail.com>
2020-07-03 15:17:42 +0530

    Update DEBUG setting

--- restapi/settings.py
+++ restapi/settings.py
@@ -85,10 +85,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'demo_1',
-        'USER': 'prashant4',
-        'PASSWORD': '143143hApPy',
-        'HOST': 'database-1.c5cddooi5xmm.us-east-2.rds.amazonaws.com',
-        'PORT': '5432',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
